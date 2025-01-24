5b7a94e5e53e6ea1f215032c50db45b7db60bf33
SiteJP Makers <sitejp01@gmail.com>
2025-01-24 13:14:15 +0900

    update

---
+++ composables/usePo.ts
@@ -0,0 +1,30 @@
+import { BG } from 'bgutils-js';
+export async function getPo(identifier: string): Promise<string | undefined> {
+    const requestKey = 'O43z0dpjhgX20SCx4KAo';
+  
+    const bgConfig = {
+      fetch: (input: string | URL | globalThis.Request, init?: RequestInit) => fetch(input, init),
+      globalObj: window,
+      requestKey,
+      identifier
+    };
+  
+    const bgChallenge = await BG.Challenge.create(bgConfig);
+  
+    if (!bgChallenge)
+      throw new Error('Could not get challenge');
+  
+    const interpreterJavascript = bgChallenge.interpreterJavascript.privateDoNotAccessOrElseSafeScriptWrappedValue;
+  
+    if (interpreterJavascript) {
+      new Function(interpreterJavascript)();
+    } else throw new Error('Could not load VM');
+  
+    const poTokenResult = await BG.PoToken.generate({
+      program: bgChallenge.program,
+      globalName: bgChallenge.globalName,
+      bgConfig
+    });
+  
+    return poTokenResult.poToken;
+  }
