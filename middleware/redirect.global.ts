781de7de06be0f7510d891977394a7340b6a1dc6
SiteJP Makers <sitejp01@gmail.com>
2025-02-26 18:38:26 +0900

    Update Redirect

--- middleware/redirect.global.ts
+++ middleware/redirect.global.ts
@@ -3,4 +3,10 @@ export default defineNuxtRouteMiddleware((to, from) => {
   if (to.path.startsWith('/@')) {
     return navigateTo(`/channel${to.path}`); // スライスしない
   }
+  if (to.path === '/gaming') {
+    return navigateTo('/channel/UCOpNcN46UbXVtpKMrmU4Abg'); // gaming.vue のリダイレクト
+  }
+  if (to.path === '/results' && to.query.search_query) {
+    return navigateTo(`/search/${to.query.search_query}`); // results.vue のリダイレクト
+  }
 });
