524f1b71cb074ae96483cbabbee2db1874adf2a9
woak973 <s221017tj@nagoya-gakuin.ed.jp>
2025-02-27 09:47:44 +0000

    Update Player

--- components/YT/Node/ChannelVideoPlayer.vue
+++ components/YT/Node/ChannelVideoPlayer.vue
@@ -55,3 +55,20 @@ const handleError = (message: string): void => {
     errorMessage.value = message;
 };
 </script>
+<style scoped>
+.video-container {
+    position: relative;
+    width: 100%;
+    padding-bottom: 56.25%;
+    /* 16:9 aspect ratio */
+    height: 0;
+}
+
+.video-container iframe {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+}
+</style>
