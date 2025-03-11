e79bae54839b9406ac0266d90f0c45eb92f0cde9
woak973 <sitejp01@gmail.com>
2025-03-11 16:30:28 +0900

    Update

---
+++ components/YT/Node/LiveChatAuthorBadge.vue
@@ -0,0 +1,16 @@
+<template>
+    <v-avatar v-if="data" :image="getProxifiedUrl(data.custom_thumbnail[0].url)" class="mr-2" rounded="0" size="14">
+    </v-avatar>
+    <div v-else>
+        No data was provided
+    </div>
+</template>
+
+<script setup lang="ts">
+
+import { YTNodes } from 'youtubei.js';
+
+const props = defineProps({
+    data: YTNodes.LiveChatAuthorBadge
+});
+</script>
---
+++ components/YT/Node/LiveChatSponsorshipsGiftPurchaseAnnouncement.vue
@@ -0,0 +1,17 @@
+<template>
+    <template v-if="data" elevation="16">
+        <YTNodeLiveChatSponsorshipsHeader :data="data.header" />
+    </template>
+    <div v-else>
+        No data was provided
+    </div>
+</template>
+
+<script setup lang="ts">
+
+import { YTNodes } from 'youtubei.js';
+
+const props = defineProps({
+    data: YTNodes.LiveChatSponsorshipsGiftPurchaseAnnouncement
+});
+</script>
