b457a0793ab6874597793f6584d1c43c87f4411e
woak973 <s221017tj@nagoya-gakuin.ed.jp>
2025-05-04 20:32:25 +0900

    fix(CommentThread): Pass 'yt' prop to YTCommonCommentView for comment handling

--- components/YT/Common/CommentView.vue
+++ components/YT/Common/CommentView.vue
@@ -2,7 +2,7 @@
   <v-list-item v-if="data">
     <v-row dense>
       <v-col cols="auto">
-        <v-list-item :to="data.author?.endpoint?.metadata?.url" style="padding: 0;">
+        <v-list-item :to="data.author?.endpoint?.metadata?.url" style="padding: 0;" @click="handleClick">
           <v-avatar :image="getProxifiedUrl(data.author?.thumbnails[0].url || '')" class="mr-2" />
         </v-list-item>
       </v-col>
@@ -34,15 +34,69 @@
   <div v-else>
     No data was provided
   </div>
+  <div>
+    <v-dialog v-model="detail" max-width="500">
+      <v-card elevation="16">
+        <v-card-title>{{ popupdata?.profileIdentityInfo?.profileIdentityInfoViewModel?.channelDisplayName }}</v-card-title>
+        <v-card-subtitle>{{ popupdata?.profileIdentityInfo?.profileIdentityInfoViewModel?.channelHandle }}</v-card-subtitle>
+        <v-card-subtitle>{{ popupdata?.profileIdentityInfo?.profileIdentityInfoViewModel?.leftOfBulletInfo }}・{{ popupdata?.profileIdentityInfo?.profileIdentityInfoViewModel?.rightOfBulletInfo }}</v-card-subtitle>
+        <v-card-actions>
+          <v-spacer />
+          <v-btn color="primary" :to="popupdata?.profileIdentityInfo?.profileIdentityInfoViewModel?.channelAccess?.buttonViewModel?.onTap?.innertubeCommand?.commandMetadata?.webCommandMetadata?.url">{{ popupdata?.profileIdentityInfo?.profileIdentityInfoViewModel?.channelAccess?.buttonViewModel?.title }}</v-btn>
+        </v-card-actions>
+        <v-card-title>{{ popupdata?.profileInfo?.profileInfoViewModel?.profileBadgeInfoChannelWide?.profileSectionTitle }}</v-card-title>
+        <v-card-subtitle>{{ popupdata?.profileInfo?.profileInfoViewModel?.profileBadgeInfoChannelWide?.commentBadge?.commentDesc }}</v-card-subtitle>
+        <template v-if="Array.isArray(popupdata?.profileInfo?.profileInfoViewModel?.profileBadgeInfoChannelWide?.badges)">
+          <v-card v-for="badge in popupdata?.profileInfo?.profileInfoViewModel?.profileBadgeInfoChannelWide?.badges" flat>
+            <v-card-text>{{ badge?.badgeDesc }}</v-card-text>
+            <v-card-subtitle>{{ badge?.badgeSubtitle }}</v-card-subtitle>
+          </v-card>
+        </template>
+        <v-card-title>{{ popupdata?.profileInfo?.profileInfoViewModel?.commentsInteractionTitle }}</v-card-title>
+        <template v-if="Array.isArray(popupdata?.profileInfo?.profileInfoViewModel?.comments)">
+          <v-card v-for="comment in popupdata?.profileInfo?.profileInfoViewModel?.comments" flat>
+            <v-card-text>{{ comment?.commentInteractionViewModel?.headerPrefix }}{{ comment?.commentInteractionViewModel?.videoTitle }}</v-card-text>
+            <v-card-subtitle>{{ comment?.commentInteractionViewModel?.commentContent }}</v-card-subtitle>
+          </v-card>
+        </template>
+        <v-card-actions>
+          <v-spacer />
+          <v-btn color="primary" @click="detail = false">Close</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </div>
 </template>
 
 <script setup lang="ts">
 
-import { YTNodes, Misc } from 'youtubei.js';
+import { YTNodes, Misc, Innertube } from 'youtubei.js';
 
-defineProps({
+const props = defineProps({
   data: YTNodes.CommentView,
+  yt: Innertube,
 });
+
+const detail = ref<boolean>(false);
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const popupdata = ref<any>();
+
+const openPopup = async () => {
+  const NE = new YTNodes.NavigationEndpoint({});
+  NE.payload = props.data?.author?.endpoint?.payload;
+  NE.name = props.data?.author?.endpoint?.name;
+  NE.metadata.api_url = '/account/get_profile_card';
+  const caller = props.yt ? props.yt : await useInnertube('common');
+  const raw = await NE.call(caller.actions);
+  detail.value = true;
+  popupdata.value = raw.data?.profileCard?.profileCardViewModel;
+};
+
+const handleClick = async () => {
+  if (!props?.data?.author?.endpoint?.metadata?.url && props?.data?.author?.endpoint?.name === 'profileCardCommand') {
+    await openPopup();
+  }
+};
 
 </script>
 
