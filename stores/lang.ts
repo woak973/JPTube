b60a59ce3c158e3c5b1e35ffdb44cd8a0fa6b5e9
woak973 <s221017tj@nagoya-gakuin.ed.jp>
2025-05-09 19:28:30 +0900

    feat(lang): Update selectedLang type to SupportedLangs and initialize lang state with getDefaultLang

--- stores/lang.ts
+++ stores/lang.ts
@@ -1,13 +1,33 @@
+export type SupportedLangs =
+  | 'af' | 'da' | 'de' | 'en-IN' | 'en-GB' | 'en' | 'es' | 'es-419' | 'es-US'
+  | 'fr' | 'fr-CA' | 'it' | 'nl' | 'pt-PT' | 'pt' | 'zh-CN' | 'zh-TW' | 'zh-HK'
+  | 'ja' | 'ko';
+
+function getDefaultLang(): SupportedLangs {
+  if (typeof navigator !== 'undefined') {
+    const browserLang = navigator.language;
+    const supportedLangs: SupportedLangs[] = [
+      'af', 'da', 'de', 'en-IN', 'en-GB', 'en', 'es', 'es-419', 'es-US',
+      'fr', 'fr-CA', 'it', 'nl', 'pt-PT', 'pt', 'zh-CN', 'zh-TW', 'zh-HK',
+      'ja', 'ko',
+    ];
+    return supportedLangs.includes(browserLang as SupportedLangs)
+      ? (browserLang as SupportedLangs)
+      : 'en';
+  }
+  return 'en';
+}
+
 export const useLangStore = defineStore('lang', {
   state: () => ({
-    lang: 'en',
+    lang: getDefaultLang(),
   }),
   actions: {
-    setLang(this: { lang: string }, newLang: string) {
+    setLang(this: { lang: string }, newLang: SupportedLangs) {
       this.lang = newLang;
     },
     resetLang(this: { lang: string }) {
-      this.lang = 'en';
+      this.lang = getDefaultLang();
     },
   },
   persist: true,
