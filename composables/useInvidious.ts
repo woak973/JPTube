4ca8f6b2f3d4ad1d7904c5512da29babe97b1b28
woak973 <s221017tj@nagoya-gakuin.ed.jp>
2025-05-18 14:42:09 +0900

    fix(Invidious): add await to fetch call in getInfo method for proper async handling

--- composables/useInvidious.ts
+++ composables/useInvidious.ts
@@ -8,7 +8,7 @@ export class Invidious {
   }
 
   async getInfo(_videoId: string): Promise<INV.VideoInfo> {
-    const info = fetch(`/api/proxy/api/v1/videos/${_videoId}?__host=${this.targetUrl}&__isSelf=true`).then(res => res.json());
+    const info = await fetch(`/api/proxy/api/v1/videos/${_videoId}?__host=${this.targetUrl}&__isSelf=true`).then(res => res.json());
     const VideoInfo = new INV.VideoInfo(info);
     return VideoInfo;
   }
