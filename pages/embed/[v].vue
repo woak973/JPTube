3d122ec784161f1c0345d5f3e85dbd7212aa2408
woak973 <s221017tj@nagoya-gakuin.ed.jp>
2025-05-19 22:01:27 +0900

    feat(Player): add `/embed` endpoint

---
+++ pages/embed/[v].vue
@@ -0,0 +1,22 @@
+<template>
+  <YTCommonPlayer
+    :key="videoId"
+    :videoId="videoId"
+    aspectRatio="fullscreen" />
+</template>
+
+<script setup lang="ts">
+
+definePageMeta({
+  layout: false,
+});
+
+const route = useRoute();
+const videoId = ref<string>(route.params.v as string);
+</script>
+
+<style>
+html, body {
+  overflow: hidden;
+}
+</style>
