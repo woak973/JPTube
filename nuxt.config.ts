ea0c12b935bd4e4b73853978773497a474327f12
woak973 <s221017tj@nagoya-gakuin.ed.jp>
2025-05-24 16:13:55 +0900

    chore(PWA): update dependencies and add PWA assets
    - Updated @nuxt/eslint from ^1.3.0 to ^1.4.1
    - Updated pinia-plugin-persistedstate from ^4.2.0 to ^4.3.0
    - Added @vite-pwa/assets-generator and @vite-pwa/nuxt as dependencies
    - Added a new script to generate PWA assets using pwa-assets-generator
    - Updated favicon.ico and added new PWA icons (apple-touch-icon-180x180.png, maskable-icon-512x512.png, pwa-192x192.png, pwa-512x512.png, pwa-64x64.png)
    - Added new icon-green.svg for PWA usage

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -13,6 +13,7 @@ export default defineNuxtConfig({
     '@pinia/nuxt',
     'pinia-plugin-persistedstate',
     '@nuxt/eslint',
+    '@vite-pwa/nuxt',
   ],
   vite: {
     server: {
@@ -27,6 +28,49 @@ export default defineNuxtConfig({
       cookie: process.env.COOKIE || '',
       self: process.env.SELF === 'false' ? false : true,
       direct: process.env.DIRECT === 'true' ? true : false,
+    },
+  },
+  pwa: {
+    registerType: 'autoUpdate',
+    manifest: {
+      name: 'JPTube',
+      description: 'A alternative YouTube frontend',
+      theme_color: '#00DC82',
+      lang: 'ja',
+      short_name: 'JPTube',
+      start_url: '/',
+      display: 'standalone',
+      background_color: '#ffffff',
+      icons: [
+        {
+          src: 'pwa-64x64.png',
+          sizes: '64x64',
+          type: 'image/png',
+        },
+        {
+          src: 'pwa-192x192.png',
+          sizes: '192x192',
+          type: 'image/png',
+        },
+        {
+          src: 'pwa-512x512.png',
+          sizes: '512x512',
+          type: 'image/png',
+        },
+        {
+          src: 'maskable-icon-512x512.png',
+          sizes: '512x512',
+          type: 'image/png',
+          purpose: 'maskable',
+        },
+      ],
+    },
+    workbox: {
+      navigateFallback: null,
+    },
+    devOptions: {
+      enabled: false,
+      type: 'module',
     },
   },
 });
