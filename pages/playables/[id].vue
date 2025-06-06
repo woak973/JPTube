f9a8a5f64672b815c1a7e395ad5c915fb793313f
woak973 <s221017tj@nagoya-gakuin.ed.jp>
2025-06-07 03:16:54 +0900

    refactor(playables): clean up unused variables and improve fetchData logic

--- pages/playables/[id].vue
+++ pages/playables/[id].vue
@@ -1,60 +1,27 @@
 <script setup lang="ts">
-import { YT, Utils, Helpers, YTNodes } from 'youtubei.js';
+import { Utils, YTNodes } from 'youtubei.js';
 import { BinaryWriter } from '@bufbuild/protobuf/wire';
 
 const route = useRoute();
-
-const results = ref<Helpers.ObservedArray<Helpers.YTNode>>();
-const HeaderResults = ref<YTNodes.C4TabbedHeader | YTNodes.CarouselHeader | YTNodes.InteractiveTabbedHeader | YTNodes.PageHeader | undefined>();
-const MetaResults = ref<YTNodes.ChannelMetadata & Partial<YTNodes.MicroformatData>>();
-const about = ref<YTNodes.ChannelAboutFullMetadata | YTNodes.AboutChannel>();
 const alert = ref<boolean>(false);
 const errorMessage = ref<string>('');
-
-watch(HeaderResults, (newVal) => {
-  if (newVal) {
-    if (newVal instanceof YTNodes.PageHeader) {
-      useHead({
-        title: `${newVal.page_title ? newVal.page_title : 'Channel'} - JPTube`,
-      });
-    } else if (newVal instanceof YTNodes.CarouselHeader) {
-      useHead({
-        title: `${(newVal.contents[1] as YTNodes.TopicChannelDetails).title.text ? (newVal.contents[1] as YTNodes.TopicChannelDetails).title.text : 'Channel'} - JPTube`,
-      });
-    } else if (newVal instanceof YTNodes.InteractiveTabbedHeader) {
-      useHead({
-        title: `${newVal.title.text ? newVal.title.text : 'Channel'} - JPTube`,
-      });
-    }
-  }
-},
-);
+// const baseUrl = `/api/playables/v/assets/index.html?__host=${route.params.id}.playables.usercontent.goog&__isSelf=true`;
+const waitingMessage = ref<string>('Setting up...');
 
 const fetchData = async () => {
   try {
     const yt = await useInnertube('common');
 
     const writer = new BinaryWriter();
-    writer.uint32(1090).fork();
+    writer.uint32(1066).fork();
     writer.uint32(10).string(route.params.id as string);
     writer.join();
     const Uint8 = writer.finish();
-    let params = encodeURIComponent(Utils.u8ToBase64(Uint8));
-    if (route.query.scp) {
-      params += route.query.scp;
-    }
+    const params = encodeURIComponent(Utils.u8ToBase64(Uint8));
 
-    const nav = new YTNodes.NavigationEndpoint({ browseEndpoint: { browseId: 'FEshopping_collection', params: params } });
+    const nav = new YTNodes.NavigationEndpoint({ browseEndpoint: { browseId: 'FEmini_app_container', params: params } });
     const ParsedResults = await nav.call(yt.actions, { parse: true });
-    const SearchResults = new YT.Channel(yt.actions, ParsedResults);
-    HeaderResults.value = SearchResults.header;
-    MetaResults.value = SearchResults.metadata as YTNodes.ChannelMetadata & Partial<YTNodes.MicroformatData>;
-    if (SearchResults.has_about) {
-      about.value = await SearchResults.getAbout();
-    }
-    if (SearchResults.current_tab?.content && 'contents' in SearchResults.current_tab.content) {
-      results.value = await SearchResults.current_tab.content.contents;
-    }
+    console.log('SearchResults', ParsedResults);
   } catch (error) {
     alert.value = true;
     if (error instanceof Error) {
@@ -65,10 +32,46 @@ const fetchData = async () => {
   }
 };
 
+/* onMounted(async () => {
+  if (!route.params.id) return;
+
+  if ('serviceWorker' in navigator) {
+    try {
+      const registration = await navigator.serviceWorker.register('/sw.js', { scope: '/api/playables' });
+      if (registration && registration.active) {
+        registration.active.postMessage({ id: route.params.id });
+        waitingMessage.value = 'Loading game...';
+        window.location.href = baseUrl;
+      } else {
+        navigator.serviceWorker.ready.then((reg) => {
+          if (reg.active) {
+            window.location.reload();
+          }
+        });
+      }
+    } catch (error) {
+      alert.value = true;
+      if (error instanceof Error) {
+        errorMessage.value = error.message;
+      } else {
+        errorMessage.value = 'An unknown error occurred';
+      }
+    }
+  }
+});
+*/
+
 await fetchData();
 </script>
+
 <template>
   <v-container>
+    <v-card v-if="route.query.id">
+      <v-card-title>{{ waitingMessage }}</v-card-title>
+    </v-card>
+    <v-card v-else>
+      <v-card-title>No id was provided</v-card-title>
+    </v-card>
     <div>
       <v-dialog v-model="alert" max-width="500">
         <v-card>
